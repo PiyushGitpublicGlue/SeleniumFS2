0a3785900e50d0e16445a2f765b7090655192382
Deployment-SYS <ttestrsys@gmail.com>
2025-11-18 18:20:11 +0530

    changes5

--- src/main/java/org/example/LaunchBrowser.java
+++ src/main/java/org/example/LaunchBrowser.java
@@ -1,13 +1,16 @@
 package org.example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LaunchBrowser {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com");
-        Thread.sleep(10000);
+        driver.get("https://demo.evershop.io/");
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//a[text()='SHOP NOW']")).click();
+        Thread.sleep(5000);
         driver.quit();
     }
 }
