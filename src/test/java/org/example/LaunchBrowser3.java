3e51c75249cb355ef63a091b354b6a563a380da1
Deployment-SYS <ttestrsys@gmail.com>
2025-11-23 08:01:59 +0530

    changes12

--- src/test/java/org/example/LaunchBrowser3.java
+++ src/test/java/org/example/LaunchBrowser3.java
@@ -2,28 +2,23 @@ package org.example;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class LaunchBrowser2 {
+public class LaunchBrowser3 {
 
     @Test(testName = "chrome", groups = {"positive"})
     public void browserLaunch() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("https://demo.evershop.io/");
         //Thread.sleep(5000);
-        WebElement button = driver.findElement(By.xpath("//a[text()='SHOP NOW']"));
-        Assert.assertEquals(button.getText(),"SHOP NOWS");
-        Assert.assertTrue(button.isDisplayed());
         driver.findElement(By.xpath("//a[text()='SHOP NOW']")).click();
         //Thread.sleep(5000);
         driver.quit();
     }
 
-    @Test(testName = "chrome", groups = {"negative"})
+    @Test(testName = "chrome", groups = {"positive"})
     public void browserLaunchFirefox() throws InterruptedException {
         WebDriver driver = new FirefoxDriver();
         driver.get("https://www.google.com");
